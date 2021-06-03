ec6a0dd5ecb479dd7e706c4fb4a825a92ec1fe29
Squair <Georgesquair@gmail.com>
2021-06-03 22:38:16 +0100

    Reverts backend syntax

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
-DOCKER_HOST='ssh://ubuntu@ubuntu'
 
 
-docker-compose rm -f
-docker-compose pull
+
+DOCKER_HOST='ssh://ubuntu@ubuntu' docker-compose rm -f
+DOCKER_HOST='ssh://ubuntu@ubuntu' docker-compose pull
 
 echo "Spinning up containers..."
-docker-compose up --build -d
+DOCKER_HOST='ssh://ubuntu@ubuntu'docker-compose up --build -d
 
 echo "Pruning images..."
-docker image prune -f
+DOCKER_HOST='ssh://ubuntu@ubuntu' docker image prune -f
 
