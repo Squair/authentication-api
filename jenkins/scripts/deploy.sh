46ee76e3fdee7e07ca67e3c4c5c70b3564e7c928
Squair <Georgesquair@gmail.com>
2021-06-05 14:23:30 +0100

    Adds detached flag back into deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -7,7 +7,7 @@ DOCKER_HOST='ssh://ubuntu@ubuntu' docker-compose rm -f
 DOCKER_HOST='ssh://ubuntu@ubuntu' docker-compose pull
 
 echo "Spinning up containers..."
-DOCKER_HOST='ssh://ubuntu@ubuntu' docker-compose up --build
+DOCKER_HOST='ssh://ubuntu@ubuntu' docker-compose up --build -d
 
 echo "Pruning images..."
 DOCKER_HOST='ssh://ubuntu@ubuntu' docker image prune -f
